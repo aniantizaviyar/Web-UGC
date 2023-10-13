c0d5137014c3902b7a751f275e99629fbb53611d
aniantizaviyar <74729993+aniantizaviyar@users.noreply.github.com>
2023-10-13 18:09:36 +0700

    Add files via upload

---
+++ sos-med.php
@@ -0,0 +1,15 @@
+			<ul class="social-icons pull-right">
+               	<li class="social-instagram">
+                   	<a href="https://www.instagram.com/ugc_logistic/?hl=id" target="_blank" title="Instagram"></a>
+               	</li>
+               	<li class="social-facebook">
+                   	<a href="https://www.facebook.com/ugclogistics" target="_blank" title="Facebook"></a>
+               	</li>
+               	<li class="social-twitter">
+               	    <a href="https://twitter.com/UGCLogistics" target="_blank" title="Twitter"></a>
+               	</li>
+               	<li class="social-linkedin">
+                   	<a href="https://www.linkedin.com/company/pt.-utama-globalindo-cargo" target="_blank" title="Linkedin"></a>
+               	</li>
+
+            </ul>
